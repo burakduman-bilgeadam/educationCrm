48985b16fbf9b611908d49d8518a9a6d3096f598
burakduman-bilgeadam <burkdumn@gmail.com>
2022-08-06 14:28:49 +0300

    crud add school

--- src/main/java/com/example/educationCrm/controller/SchoolController.java
+++ src/main/java/com/example/educationCrm/controller/SchoolController.java
@@ -1,14 +1,14 @@
 package com.example.educationCrm.controller;
 
 import com.example.educationCrm.model.dto.SchoolDTO;
+import com.example.educationCrm.model.dto.SchoolInformationDTO;
 import com.example.educationCrm.service.SchoolService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/school")
@@ -21,5 +21,22 @@ public class SchoolController {
         this.schoolService.save(schoolDTO.getName());
         return new ResponseEntity("Başarılı İşlem"
                 ,HttpStatus.CREATED);
+    }
+
+    @PutMapping
+    public ResponseEntity update(@RequestBody SchoolDTO schoolDTO){
+        this.schoolService.update(schoolDTO);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping
+    public ResponseEntity delete(@RequestBody SchoolDTO schoolDTO){
+        this.schoolService.delete(schoolDTO);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping
+    public List<SchoolInformationDTO> getAllName(){
+        return this.schoolService.findAllName();
     }
 }
--- src/main/java/com/example/educationCrm/repository/SchoolRepository.java
+++ src/main/java/com/example/educationCrm/repository/SchoolRepository.java
@@ -1,10 +1,15 @@
 package com.example.educationCrm.repository;
 
+import com.example.educationCrm.model.dto.SchoolInformationDTO;
 import com.example.educationCrm.model.entity.School;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface SchoolRepository
-        extends CrudRepository<School,Long> {
+        extends JpaRepository<School,Long> {
+    List<SchoolInformationDTO> getAllById(Long id);
 }
